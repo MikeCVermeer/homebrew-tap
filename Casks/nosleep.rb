eedaf9aba32e2551d4dcef6a58cac95f5e39d66a
Mike Vermeer <77960135+MikeCVermeer@users.noreply.github.com>
2026-05-05 21:11:05 +0200

    Clean up NoSleep cask URL

--- Casks/nosleep.rb
+++ Casks/nosleep.rb
@@ -2,8 +2,7 @@ cask "nosleep" do
   version "1.0"
   sha256 "82387a9c080dbb4819babc2356fc652d0442e9b1d4162ad769d00d7698faf782"
 
-  url "https://github.com/MikeCVermeer/NoSleep/releases/download/v#{version}/NoSleep-#{version}.zip?download=1",
-      verified: "github.com/MikeCVermeer/NoSleep/"
+  url "https://github.com/MikeCVermeer/NoSleep/releases/download/v#{version}/NoSleep-#{version}.zip?download=1"
   name "NoSleep"
   desc "Native macOS menu-bar utility that keeps your Mac awake"
   homepage "https://github.com/MikeCVermeer/NoSleep"
